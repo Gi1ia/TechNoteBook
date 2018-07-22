9ecf2d4331f75b6db7692176a1fdc6d2068bf533
unknown <Gi1ia>
2018-07-22 15:36:59 -0700

    Add three sum

--- 3sum.py
+++ 3sum.py
@@ -11,11 +11,71 @@ The solution set must not contain duplicate triplets.
 """
 
 class Solution(object):
-    def threeSum(self, nums):
+    def threeSum_no_deDup(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
+        :Note: This solution will contain duplicated answer. 
         """
         nums.sort()
+
+        # if we change res to a set: res = set()
+        # and return the set in list format, the de-dup problem will be solved.
+        # 1. return list(res)
+        # 2. return map(list, res)
         res = []
         
+        for i in range (0, len(nums) - 2): # exclude last two indexes
+          if i > 0 and nums[i] == nums[i - 1]: # first index should be identical
+            continue
+          left = i + 1
+          right = len(nums) - 1
+          while left < right:
+            current_sum = nums[i] + nums[left] + nums[right]
+            if current_sum < 0:
+              left += 1
+            elif current_sum > 0:
+              right -= 1
+            else:
+              res.append((nums[i], nums[left], nums[right]))
+              left += 1
+              right -= 1
+        
+        return res
+
+    def threeSum(self, nums):
+      """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+      nums.sort()
+      res = []
+
+      for i in range (0, len(nums) - 2):
+        if i > 0 and nums[i] == nums[i - 1]:
+          continue
+        left = i + 1
+        right = len(nums) - 1
+        while left < right:
+          current_sum = nums[i] + nums[left] + nums[right]
+          if current_sum < 0:
+            left += 1
+          elif current_sum > 0:
+            right -= 1
+          else:
+            res.append((nums[i], nums[left], nums[right]))
+            # by checking the next index value to solve de-dup.
+            while left < right and nums[left] == nums[left + 1]:
+              left += 1
+            while left < right and nums[right -1] == nums[right]:
+              right -= 1
+            # Note: Following code is for normal cases. 
+            # Without the following line, the loop will be infinite.
+            left, right = left + 1, right - 1
+      
+      return res
+
+s = Solution()
+print (s.threeSum([-2,0,0,2,2]))
+print (s.threeSum_no_deDup([-2,0,0,2,2]))
+input("Press Enter")
