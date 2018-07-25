17acfbbc3e9bde3e8498932da23c0a9997f68b31
Weijia Wang <Gi1ia>
2018-07-25 16:29:40 -0700

    add meeting rooms question

---
+++ MeetingRooms.py
@@ -0,0 +1,25 @@
+"""
+Given an array of meeting time intervals consisting of start and end times 
+[[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
+
+Example 1:
+Input: [[0,30],[5,10],[15,20]]
+Output: false
+
+Example 2:
+Input: [[7,10],[2,4]]
+Output: true
+"""
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution(object):
+    def canAttendMeetings(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: bool
+        """
+        
