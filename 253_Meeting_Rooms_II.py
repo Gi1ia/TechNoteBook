6739908c3b517301402027c48d6a7263d23ea512
unknown <41492334+Gi1ia@users.noreply.github.com>
2018-07-29 14:08:43 -0700

    Add TODO problems

---
+++ 253_Meeting_Rooms_II.py
@@ -0,0 +1,26 @@
+"""
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+
+Example 1:
+
+Input: [[0, 30],[5, 10],[15, 20]]
+Output: 2
+Example 2:
+
+Input: [[7,10],[2,4]]
+Output: 1
+#Facebook #Google #Snapchat #Heap #Greedy
+"""
+# Definition for an interval.
+# class Interval:
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution:
+    def minMeetingRooms(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: int
+        """
+        
