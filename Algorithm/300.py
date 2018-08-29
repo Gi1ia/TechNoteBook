d07c4a7fe94c53dd9d799a74f36ae5cacfc98cec
G1i1a <41492334+Gi1ia@users.noreply.github.com>
2018-08-29 14:23:32 -0700

    Adding two p that looking for longest something

---
+++ Algorithm/300.py
@@ -0,0 +1,16 @@
+class Al():
+	
+	def lis(self, nums):
+		if len(nums) <= 1:
+			return len(nums)
+
+		# Current[i] will store the length of LIS that end with nums[i]
+		current = [1 for _ in range(len(nums))]
+		for i in range(1, len(nums)):
+			for j in range(0, i):
+				# It means nums[i] can contribute to current[j] by 1
+				# And the new LIS will end with nums[i
+				if nums[j] < nums[i]:
+					current[i] = max(current[i], current[j] + 1)
+
+		return max(current)
