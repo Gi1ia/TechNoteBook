d43d689890629e79e24441a07c15f601de0199b8
G1i1a <41492334+Gi1ia@users.noreply.github.com>
2018-08-30 15:21:29 -0700

    Add bfs solution to 200 island

--- Algorithm/200_Number_Of_Island.py
+++ Algorithm/200_Number_Of_Island.py
@@ -1,3 +1,5 @@
+import collections
+
 class Solution:
     def numIslands(self, grid):
         """
@@ -26,4 +28,22 @@ class Solution:
         self.dfs(grid, x, y + 1)
 
     def numIslands_BFS(self, grid):
-        pass
+        
+        queue = collections.deque()
+        count = 0
+
+        for i in range(len(grid)):
+        	for j in range(len(grid[0])):
+        		if grid[i][j] == "1":
+        			count += 1
+        			grid[i][j] = "#"
+        			queue.extend([(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)])
+        			while queue:
+        				x, y = queue.popleft()
+        				if x < 0 or y < 0 or x >= len(grid) or \
+        				y >= len(grid[0]) or grid[x][y] != "1":
+        					continue
+        				grid[x][y] = "#"
+        				queue.extend([(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)])
+
+        return count
