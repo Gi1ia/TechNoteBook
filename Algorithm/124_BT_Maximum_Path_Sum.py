6317ac774ca25a1e7245bd8d1f5c8872bbe77770
gi1ia <41492334+Gi1ia@users.noreply.github.com>
2018-09-12 17:05:27 +0800

    Fix

--- Algorithm/124_BT_Maximum_Path_Sum.py
+++ Algorithm/124_BT_Maximum_Path_Sum.py
@@ -6,9 +6,28 @@
 #         self.right = None
 
 class Solution:
-    def maxPathSum(self, root):
+    def __init__(self):
+        self.sum = float('-inf')
+
+    def maxPathSum_recursive(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
+        self.max_sum_down(root)
+
+        return self.sum
+    
+    def max_sum_down(self, node):
+        """ Calculate the max sum from node to its leaves
+        """
+        if not node:
+            return 0
         
+        max_left = max(0, self.max_sum_down(node.left))
+        max_right = max(0, self.max_sum_down(node.right))
+        # UPdate global sum
+        self.sum = max(self.sum, max_left + max_right + node.val)
+
+        # Return the single path sum
+        return max(max_left, max_right) + node.val
