6cfe6cc6249024404a59a0232ee6f26535d63f90
gi1ia <41492334+Gi1ia@users.noreply.github.com>
2018-09-15 16:48:37 +0800

    max distance to closed person

---
+++ Algorithm/849_Max_Distance_To_Closest_People.py
@@ -0,0 +1,56 @@
+class Solution:
+    def maxDistToClosest2(self, seats):
+        """
+        :type seats: List[int]
+        :rtype: int
+        """
+        prev = -1
+        res = float('-inf')
+        if seat[0] == 1:
+            prev = 0
+
+        for i, seat in enumerate(seats):
+            if i == 1:
+                if j == -1:
+                    res = max(res, i)
+                else:
+                    res = max(res, (i - j) // 2)
+                j = i
+        
+        if seat[-1] == 0:
+            res = max(res, len(seats) -1 - j)
+            
+        return res
+
+    def maxDistToClosest(self, seats):
+        """
+        :type seats: List[int]
+        :rtype: int
+        """
+        if not seats:
+            return None
+        
+        if len(seats) == 1:
+            return 0 if seats[0] == 0 else None
+        
+        seats.insert(0, float('-inf'))
+        seats.append(float('inf')) # length of seats become n + 2
+        prev = 0
+        max_dist = -1
+        
+        for current, seat in enumerate(seats):
+            if seat == 1:
+                dist = (current - prev)//2 if prev != 0 else current - 1 # -1 because we insert inf at index 0
+                if dist > max_dist:
+                    max_dist = max(max_dist, dist)
+                prev = current
+            elif seat == float('inf'):
+                dist = current - prev - 1 # -1 because we append inf to the end
+                if dist > max_dist:
+                    max_dist = max(max_dist, dist)           
+        
+        return max_dist 
+
+seats = [1,0,0,0]
+s = Solution()
+print(s.maxDistToClosest(seats))
