a8eff04f1a7682fe08eb8bcc3008f3f7d30573d7
gi1ia <41492334+Gi1ia@users.noreply.github.com>
2018-09-15 13:58:02 +0800

    Fix typo!

--- Algorithm/855_Exam_Room.py
+++ Algorithm/855_Exam_Room.py
@@ -5,13 +5,13 @@ class ExamRoom(object):
         self.N = N
         self.students = []
         self.heap = []
-        self.avail_first = {}
-        self.avail_last = {}
+        self.avail_first = {} # used later in leave()
+        self.avail_last = {} # used later in leave()
         self.put_seg(0, self.N - 1) # Initialize with empty room
     
     def seat(self):
-        while  True:
-            _, fisrt, last, is_avail = heappop(self.heap)
+        while True:
+            _, first, last, is_avail = heapq.heappop(self.heap)
             if is_avail: # delete
                 del self.avail_last[last]
                 del self.avail_first[first]
@@ -28,11 +28,11 @@ class ExamRoom(object):
                 self.put_seg(first, last - 1)
         
         else:
-            res = (last - first) // 2 + fisrt 
+            res = (last - first) // 2 + first 
             if res > first: 
                 self.put_seg(first, res - 1)
             if res < last: # break the segment and put both into heap
-                self.put_seg(first + 1, res)
+                self.put_seg(res + 1, last)
         
         return res
     
@@ -42,12 +42,12 @@ class ExamRoom(object):
 
         first, last = p, p # Default value, incase p == 0 or p == N - 1
 
-        if left > 0 and left in self.avail_last:
+        if left >= 0 and left in self.avail_last:
             seg_left = self.avail_last.pop(left)
-            fisrt = seg_left[1]
-            seg_right[3] = False
+            first = seg_left[1]
+            seg_left[3] = False
         
-        if right < self.N - 1 and right in self.avail_first:
+        if (right <= self.N - 1) and right in self.avail_first:
             seg_right = self.avail_first.pop(right)
             last = seg_right[2]
             seg_right[3] = False
@@ -55,18 +55,16 @@ class ExamRoom(object):
         self.put_seg(first, last)
 
     def put_seg(self, first, last):
-        if first == 0:
-            l = last - first
-        elif last == self.N - 1:
+        if first == 0 or last == self.N - 1:
             l = last - first
         else:
             l = (last - first) // 2
         
-        segment = [-l, fisrt, last, True]
+        segment = [-l, first, last, True]
         self.avail_last[last] = segment # pass by ref?
         self.avail_first[first] = segment
 
-        heappush(self.heap, segment)
+        heapq.heappush(self.heap, segment)
 
     def seat_ON(self):
         """ Seat function
@@ -96,5 +94,10 @@ class ExamRoom(object):
         """
         self.students.remove(p)
 
+
+# Your ExamRoom object will be instantiated and called as such:
+# obj = ExamRoom(N)
+# param_1 = obj.seat()
+# obj.leave(p)
 
 # ref: [heap solution](https://leetcode.com/problems/exam-room/discuss/139941/Python-O(log-n)-time-for-both-seat()-and-leave()-with-heapq-and-dicts-Detailed-explanation)
