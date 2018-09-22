ed911b4051cf14c0838d449bec6854acb9d759f1
gi1ia <41492334+Gi1ia@users.noreply.github.com>
2018-09-21 19:15:37 -0700

    some draft

--- Algorithm/9904_Divide_G.py
+++ Algorithm/9904_Divide_G.py
@@ -3,10 +3,37 @@
 """
 class Solution():
     def divide(self, divdent, divisor):
-        """
+        """ divdent/divisor
         divdent: int
         divisor: int
         return: tuple(string, string)
         """
+        if not divisor: # illegal
+            return None 
+
+        have_seen = dict{}
+        first, second = [], []
+
+        step = 0
+        quotient = divdent // divisor
+        remainder = divdent % divisor
+        first.append(str(quotient))
         
+        # Early return
+        if remainder == 0:
+            return (str(quotient), "")       
+
         
+
+        while remainder not in have_seen:
+            have_seen[remainder] = step
+            second.append(quotient)
+            second += 1
+            divdent = remainder * 10
+            quotient = divdent // divisor
+            remainder = divdent % divisor
+        
+
+            
+
+
