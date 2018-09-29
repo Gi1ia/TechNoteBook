a3c46cacae418132dc56229cb291b85df2a1edd8
unknown <41492334+Gi1ia@users.noreply.github.com>
2018-09-29 10:45:26 -0700

    Fix bug

--- Algorithm/9005_Submarine.py
+++ Algorithm/9005_Submarine.py
@@ -1,3 +1,5 @@
+import collections
+
 class HitSubmarine():
     def __init__(self, sbm, speed):
         self.sbm_x = sbm[0]
@@ -8,27 +10,49 @@ class HitSubmarine():
     
     def shoot_bullet(self):
         directions = [(0, 1), (-1, 0), (0, -1), (1, 0)]
-        step = 1
-        d = 0
+        possible_v = 1 # Assume submarine won't stay still
+        time = 1
+        d = 0 # cursor for direction
 
-        while True: # Could be infinite                   
-            for s in range(step):
-                if self.hit():
-                    return (self.blt_x, self.blt_y)
-                self.blt_x += directions[d][0]
-                self.blt_y += directions[d][1]
-                self.move_sbm()
+        while True:
+            x, y = directions[d]
+            x = x * time * possible_v
+            y = y * time * possible_v
+            self.blt_x = x
+            self.blt_y = y
+            if self.hit():
+                break
             
-            # Change step
-            if d == 1 or d == 3:
-                step += 1
-
-            # change direction
+            # doesn't hit. Time ++
+            self.move_sbm
+            time += 1
             d += 1
-            d %= 4 
-        
-        return (float('inf'), float('inf'))
+            d %= 4
+            possible_v += 1
 
+        
+        return (directions[d], possible_v)
+
+        # Following previous version with wrong approach
+        """
+            step = 1
+            while True: # Could be infinite                   
+                for s in range(step):
+                    if self.hit():
+                        return (self.blt_x, self.blt_y)
+                    self.blt_x += directions[d][0]
+                    self.blt_y += directions[d][1]
+                    self.move_sbm()
+                
+                # Change step
+                if d == 1 or d == 3:
+                    step += 1
+
+                # change direction
+                d += 1
+                d %= 4 
+        """
+        
     def move_sbm(self):
         self.sbm_x += self.speed[0]
         self.sbm_y += self.speed[1]
