3cea4d053c8946e1442d016ed23f3963aff1e2ba
gi1ia <41492334+Gi1ia@users.noreply.github.com>
2018-10-14 22:00:35 -0700

    Edit 767 and 923

--- Algorithm/767_Reorganize_String.py
+++ Algorithm/767_Reorganize_String.py
@@ -1,5 +1,49 @@
+import collections
+import heapq
+
 class Solution:
     def reorganizeString(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        if not S:
+            return ""
+
+        char_count = collections.defaultdict(int)
+        for c in S:
+            char_count[c] += 1
+        
+        h = []
+        for key in char_count:
+            heapq.heappush(h, (-char_count[key], key))
+
+        # Early return when there is no valid solution
+        if any(-count > (len(S) + 1) / 2 for count, x in h):
+            return ""
+
+        res = []
+        i = 0
+        while len(h) >= 2:
+            count1, c1 = heapq.heappop(h)
+            count2, c2 = heapq.heappop(h)
+            # Since Python will also compare letter alpha beta
+            # We can make sure that c1, c2 will always in order
+            # So below code coulde be eliminated:
+            #if not ans or ch1 != ans[-1]:
+            #    ans.extend([ch1, ch2])
+            #else:
+            #    ans.extend([ch2, ch1])
+            res.extend([c1, c2])
+            if count1 + 1 < 0:
+                heapq.heappush(h, (count1 + 1, c1))
+            if count2 + 1 < 0:
+                heapq.heappush(h, (count2 + 1, c2))
+            
+        return "".join(res) + (h[0][1] if h else "")
+
+    
+    def reorganizeString_naive_approach(self, S):
         """
         :type S: str
         :rtype: str
--- Algorithm/923_3_Sum_With_Multiplicity.py
+++ Algorithm/923_3_Sum_With_Multiplicity.py
@@ -1,34 +1,61 @@
 """
-Given an integer array A, and an integer target, return the number of tuples i, j, k
-such that i < j < k and A[i] + A[j] + A[k] == target.
+    Given an integer array A, and an integer target, return the number of tuples i, j, k
+    such that i < j < k and A[i] + A[j] + A[k] == target.
 
-As the answer can be very large, return it modulo 10^9 + 7.
+    As the answer can be very large, return it modulo 10^9 + 7.
 
-Example 1:
-Input: A = [1,1,2,2,3,3,4,4,5,5], target = 8
-Output: 20
-Explanation: 
-Enumerating by the values (A[i], A[j], A[k]):
-(1, 2, 5) occurs 8 times;
-(1, 3, 4) occurs 8 times;
-(2, 2, 4) occurs 2 times;
-(2, 3, 3) occurs 2 times.
+    Example 1:
+    Input: A = [1,1,2,2,3,3,4,4,5,5], target = 8
+    Output: 20
+    Explanation: 
+    Enumerating by the values (A[i], A[j], A[k]):
+    (1, 2, 5) occurs 8 times;
+    (1, 3, 4) occurs 8 times;
+    (2, 2, 4) occurs 2 times;
+    (2, 3, 3) occurs 2 times.
 
-Example 2:
-Input: A = [1,1,2,2,2,2], target = 5
-Output: 12
-Explanation: 
-A[i] = 1, A[j] = A[k] = 2 occurs 12 times:
-We choose one 1 from [1,1] in 2 ways,
-and two 2s from [2,2,2,2] in 6 ways.
+    Example 2:
+    Input: A = [1,1,2,2,2,2], target = 5
+    Output: 12
+    Explanation: 
+    A[i] = 1, A[j] = A[k] = 2 occurs 12 times:
+    We choose one 1 from [1,1] in 2 ways,
+    and two 2s from [2,2,2,2] in 6 ways.
 
-Note:
-3 <= A.length <= 3000
-0 <= A[i] <= 100
-0 <= target <= 300
+    Note:
+    3 <= A.length <= 3000
+    0 <= A[i] <= 100
+    0 <= target <= 300
 """
-
+import collections
 class Solution:
+    def threeSumMulti(self, A, target):
+        """
+        :type A: List[int]
+        :type target: int
+        :rtype: int
+        """
+        single_sum = collections.defaultdict(int) # track the count of single elements seen so far
+        two_sum = collections.defaultdict(int) # track the count of the sum of any two elements seen so far
+
+        N = len(A)
+        MOD = 10 ** 9 + 7
+        res = 0
+        for i in range(N - 1, -1, -1): # why reversed?
+            current = A[i]
+            res += two_sum[target - current]
+            res %= MOD
+            for (num, cnt) in single_sum.items():
+                two_sum[num + current] += cnt
+
+            single_sum[current] += 1
+        
+        return res
+
+            
+
+
+
     def threeSumMulti_TLE(self, A, target):
         """
         :type A: List[int]
