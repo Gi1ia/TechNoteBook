eaaa25dfd719760f4e393f36a22831db7c948942
unknown <41492334+Gi1ia@users.noreply.github.com>
2018-10-26 17:21:58 -0700

    DP practice 85

--- Algorithm/085_Maximal_Rectangle.py
+++ Algorithm/085_Maximal_Rectangle.py
@@ -1,5 +1,54 @@
 class Solution:
-    def maximalRectangle(self, matrix):
+    def maximalRectangle_dp(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if not matrix:
+            return 0
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        height = [[0 for x in range(n)] for x in range(m)]
+        left = [[0 for x in range(n)] for x in range(m)]
+        right = [[n for x in range(n)] for x in range(m)]
+
+        res = 0
+
+        for i in range(m):
+            currLeft = 0
+            currRight = n
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    if i == 0:
+                        height[i][j] = 1
+                        left[i][j] = currLeft
+                    else:
+                        height[i][j] = height[i - 1][j] + 1
+                        left[i][j] = max(left[i - 1][j], currLeft)
+                else:
+                    height[i][j] = 0
+                    left[i][j] = 0
+                    currLeft = j + 1
+
+            for j in range(n - 1, -1, -1):
+                if matrix[i][j] == '1':
+                    if i == 0:
+                        right[i][j] = currRight
+                    else:
+                        right[i][j] = min(right[i - 1][j], currRight)
+                else:
+                    right[i][j] = n  # because the height from next row won't reach this row
+                    currRight = j
+
+            for j in range(n):
+                res = max(res, height[i][j] * (right[i][j] - left[i][j]))
+
+        return res
+
+
+    def maximalRectangle_save_space(self, matrix):
         """
         :type matrix: List[List[str]]
         :rtype: int
