aad482b14bc22ae15e0599f19e379d6532b8e58e
G1i1a <41492334+Gi1ia@users.noreply.github.com>
2018-11-06 09:37:38 -0800

    test local

--- Design/Web Sockets.py
+++ Design/Web Sockets.py
@@ -17,6 +17,22 @@ start_server = websockets.serve(time, '127.0.0.1', 5678)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
 
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print(f"< {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f"> {greeting}")
+
+"""
+start_server = websockets.serve(hello, 'localhost', 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+"""
+
 
 # Use following html in browser to talk to the service
 """
