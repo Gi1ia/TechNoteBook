e9f73afbaa2f287c234334a643b248840be2fe52
gi1ia <41492334+Gi1ia@users.noreply.github.com>
2018-11-18 20:15:05 -0800

    Polish

---
+++ Algorithm/9811_Skiing.py
@@ -0,0 +1,8 @@
+class SkiScore():
+    def ski(self, nodes, edges):
+        """
+        nodes: Nodes and corresponding scores {A: 5, B: 7, C: 10}
+        edges: Neighbors of each nodes {A: [(B, 3), (C, 4)], B:[(D, 2), (F, 1)]}
+        """
+        
+
