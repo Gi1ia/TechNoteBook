51f292626e966f202bfc802e42c1313202c2712f
gi1ia <41492334+Gi1ia@users.noreply.github.com>
2018-11-19 23:38:30 -0800

    BFS skiing

--- Algorithm/9811_Skiing.py
+++ Algorithm/9811_Skiing.py
@@ -1,8 +1,26 @@
 class SkiScore():
-    def ski(self, nodes, edges):
+    def ski(self, nodes, edges, start, end):
         """
         nodes: Nodes and corresponding scores {A: 5, B: 7, C: 10}
-        edges: Neighbors of each nodes {A: [(B, 3), (C, 4)], B:[(D, 2), (F, 1)]}
+        edges: Neighbors of each nodes {A: { B:3, C:4 }, B: { D:2, F:1 } }
         """
-        
+        visited = set()
+        visited.add(start)
+        queue = collections.deque()
+        queue.append((start, nodes[start]))
+
+        scores = collections.defaultdict(int)
+        scores[start] = nodes[start]
+        while queue:
+            current = queue.popleft()
+            if current[0] == end:
+                return current[1]
+            if scores[current] > current[1]: # we are looking for largest score
+                continue
+            for next_node, cost in edges[current].items():
+                score = current[1] - cost + nodes[next_node]
+                if score > scores[next_node]:
+                    queue.append((next_node, score))
+
+        return float('-inf')
 
