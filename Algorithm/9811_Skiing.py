a1e8f1b033159f57a96d32da880279987ef4834a
G1i1a <41492334+Gi1ia@users.noreply.github.com>
2018-11-20 15:59:14 -0800

    Skiing problem

--- Algorithm/9811_Skiing.py
+++ Algorithm/9811_Skiing.py
@@ -1,11 +1,13 @@
+from heapq import heappush, heappop, heapify
+import collections
+
 class SkiScore():
     def ski(self, nodes, edges, start, end):
         """
         nodes: Nodes and corresponding scores {A: 5, B: 7, C: 10}
         edges: Neighbors of each nodes {A: { B:3, C:4 }, B: { D:2, F:1 } }
         """
-        visited = set()
-        visited.add(start)
+        #TODO: check again. The answer is wrong.
         queue = collections.deque()
         queue.append((start, nodes[start]))
 
@@ -23,4 +25,42 @@ class SkiScore():
                     queue.append((next_node, score))
 
         return float('-inf')
+
+    def ski_dijkstra_Bug(self, nodes, edges, start, end):
+        #NOTE: Dijkstra can NOT solve this problem
+        # Because we are looking for a biggest score it's like a negative weight problem
+        # The only way we can make sure to find the 'heaviest' weight route is by BFS/DFS
+        # We MUST complete the search.
+        # I keep this just because it could be a dijastra example.
+        queue = []
+        heappush(queue, (-nodes[start], start))
+
+        scores = collections.defaultdict(int)
+        scores[start] = nodes[start]
+
+        while queue:
+            current_score, current_node = heappop(queue)
+            current_score = -current_score
+            if current_node == end:
+                return current_score
+
+            if scores[current_node] > current_score:
+                continue
+
+            for next_node, cost in edges[current_node].items():
+
+                score = current_score - cost + nodes[next_node]
+                if score > scores[next_node]:
+                    heappush(queue, (-score, next_node))
+
+        return float('-inf')
+
+nodes = {"A":5, "B":7, "C":6, "D":2, "E":4, "F":7, "H":7, "I":3, "J":2}
+edges = {"A":{"B":2, "C":3}, "B":{"D":5, "E":6}, "C":{"E":4, "F":4},\
+ "D":{"H":7}, "E":{"H":6}, "H":{"I":1, "J":2}, "F":{"J":3}}
+start = "A"
+end = "J"
+
+obj = SkiScore()
+print(obj.ski_dijkstra_Bug(nodes, edges, start, end))
 
