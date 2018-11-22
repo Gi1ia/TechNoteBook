adf39b25d34f7b8c269944fd60d261a023218740
unknown <41492334+Gi1ia@users.noreply.github.com>
2018-11-21 23:16:28 -0800

    Review bell-man ford

--- Algorithm/787_Cheapest_Flight_With_K_Stop.py
+++ Algorithm/787_Cheapest_Flight_With_K_Stop.py
@@ -9,12 +9,12 @@ class Solution(object):
         :rtype: int
         """
         # Bell-man Ford solution
-        dp = [[float('inf') for _ in range(n)] for _ in range(K + 2)]
-        dp[0][src] = 0      
+        dp = [[float('inf') for _ in range(n)] for _ in range(K + 2)] 
+        dp[0][src] = 0 # NOTE: remember to initialize start
         
         # f[k][v] = min(f[k][v], f[k-i][u] + f[i][v]) for i in range(1, k)
         for i in range(1, K + 2):
-            dp[i][src] = 0
+            dp[i][src] = 0 # from src to src is always 0
             for city1, city2, fee in flights:
                 dp[i][city2] = min(dp[i][city2], dp[i - 1][city1] + fee)
         
